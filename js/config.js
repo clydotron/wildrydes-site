b1e68f91e911242b485f11c2bb50154a5a1230bc
clydotron <clydotron@users.noreply.github.com>
2025-07-27 15:41:34 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_QR45wKsff'
-        userPoolClientId: '42ss1djb6m6q4ac2mkn03uaem7'
-        region: 'us-east-2'
+        userPoolId: 'us-east-2_QR45wKsff',
+        userPoolClientId: '42ss1djb6m6q4ac2mkn03uaem7',
+        region: 'us-east-2',
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
